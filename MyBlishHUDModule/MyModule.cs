c302b427e33dc03c569a6fbb2292c1ab29ae97f8
Steven Liekens <steven.liekens@gmail.com>
2024-11-10 17:41:31 +0100

    Add files

---
+++ MyBlishHUDModule/MyModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Blish_HUD;
+using Blish_HUD.Modules;
+
+namespace MyBlishHUDModule;
+
+[Export(typeof(Module))]
+public class MyModule : Module
+{
+	private static readonly Logger Logger = Logger.GetLogger<MyModule>();
+
+	[ImportingConstructor]
+	public MyModule([Import("ModuleParameters")] ModuleParameters parameters)
+		: base(parameters)
+	{
+	}
+}
