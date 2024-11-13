9806e230b43a4ad42849038b82d98ad788b4c80a
Steven Liekens <steven.liekens@gmail.com>
2024-11-13 16:51:24 +0100

    Add a search box

--- ChatLinksModule/UI/Tabs/Items/ItemsView.cs
+++ ChatLinksModule/UI/Tabs/Items/ItemsView.cs
@@ -6,6 +6,8 @@ namespace ChatLinksModule.UI.Tabs.Items;
 
 public class ItemsView : View
 {
+	private TextBox _searchBox;
+
 	private ItemsView()
 	{
 	}
@@ -17,5 +19,16 @@ public class ItemsView : View
 
 	protected override void Build(Container buildPanel)
 	{
+		_searchBox = new TextBox
+		{
+			Parent = buildPanel
+		};
+
+		_searchBox.TextChanged += SearchInput;
+	}
+
+	private async void SearchInput(object sender, System.EventArgs e)
+	{
+		ScreenNotification.ShowNotification(_searchBox.Text);
 	}
 }
