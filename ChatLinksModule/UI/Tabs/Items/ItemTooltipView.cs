aed18e131336bb16d5be9050bcffc847b7e446d6
Steven Liekens <steven.liekens@gmail.com>
2024-12-05 22:02:32 +0100

    Add vendor value

--- ChatLinksModule/UI/Tabs/Items/ItemTooltipView.cs
+++ ChatLinksModule/UI/Tabs/Items/ItemTooltipView.cs
@@ -1,10 +1,12 @@
 ﻿using Blish_HUD;
 using Blish_HUD.Common.UI.Views;
+using Blish_HUD.Content;
 using Blish_HUD.Controls;
 using Blish_HUD.Graphics.UI;
 
 using ChatLinksModule.UI.Tabs.Items.Controls;
 
+using GuildWars2;
 using GuildWars2.Items;
 
 using Microsoft.Xna.Framework;
@@ -28,6 +30,7 @@ public class ItemTooltipView(Item item) : View, ITooltipView
         CreateHeader(item, layout);
         CreateDescription(item, layout);
         CreateBinding(item, layout);
+        CreateVendorValue(item, layout);
 
 
         static void CreateHeader(Item item, Container parent)
@@ -84,6 +87,11 @@ public class ItemTooltipView(Item item) : View, ITooltipView
 
         static void CreateBinding(Item item, Container parent)
         {
+            if (item.Flags.NoSell || item is Service)
+            {
+                return;
+            }
+
             if (item.Flags.Soulbound)
             {
                 var binding = new Label
@@ -125,6 +133,51 @@ public class ItemTooltipView(Item item) : View, ITooltipView
                 };
             }
         }
-    }
 
+        static void CreateVendorValue(Item item, Container parent)
+        {
+            if (item.Flags.NoSell || item.VendorValue == Coin.Zero)
+            {
+                return;
+            }
+
+            var builder = new FormattedLabelBuilder()
+                .SetWidth(parent.Width)
+                .AutoSizeHeight();
+
+            int totalCopper = item.VendorValue.Amount;
+            int goldAmount = totalCopper / 10_000;
+            totalCopper %= 10_000;
+            int silverAmount = totalCopper / 100;
+            int copperAmount = totalCopper % 100;
+
+            if (item.VendorValue.Amount >= 10_000)
+            {
+                var gold = builder.CreatePart(goldAmount.ToString());
+                gold.SetTextColor(new Color(0xDD, 0xBB, 0x44));
+                gold.SetFontSize(ContentService.FontSize.Size14);
+                gold.SetSuffixImage(AsyncTexture2D.FromAssetId(156904));
+                builder.CreatePart(gold);
+            }
+
+            if (item.VendorValue.Amount >= 100)
+            {
+                var silver = builder.CreatePart(silverAmount.ToString());
+                silver.SetTextColor(new Color(0xC0, 0xC0, 0xC0));
+                silver.SetFontSize(ContentService.FontSize.Size14);
+                silver.SetSuffixImage(AsyncTexture2D.FromAssetId(156907));
+                builder.CreatePart(silver);
+            }
+
+            var copper = builder.CreatePart(copperAmount.ToString());
+            copper.SetTextColor(new Color(0xCD, 0x7F, 0x32));
+            copper.SetFontSize(ContentService.FontSize.Size14);
+            copper.SetSuffixImage(AsyncTexture2D.FromAssetId(156902));
+            builder.CreatePart(copper);
+
+            var label = builder.Build();
+            label.Parent = parent;
+            label.Width = parent.Width;
+        }
+    }
 }
