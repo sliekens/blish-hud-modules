bc31ca92442ac092029fe056d918eedbe761e951
Steven Liekens <steven.liekens@gmail.com>
2024-12-05 01:28:10 +0100

    Tweak rarity colors

--- ChatLinksModule/UI/Tabs/Items/ItemColors.cs
+++ ChatLinksModule/UI/Tabs/Items/ItemColors.cs
@@ -13,10 +13,10 @@ public static class ItemColors
         {
             GuildWars2.Items.Rarity.Junk => new Color(0x99, 0x99, 0x99),
             GuildWars2.Items.Rarity.Basic => Color.White,
-            GuildWars2.Items.Rarity.Fine => new Color(0x44, 0x99, 0xEE),
-            GuildWars2.Items.Rarity.Masterwork => new Color(0x22, 0xBB, 0x11),
-            GuildWars2.Items.Rarity.Rare => new Color(0xEE, 0xDD, 0x22),
-            GuildWars2.Items.Rarity.Exotic => new Color(0xEE, 0x99, 0x00),
+            GuildWars2.Items.Rarity.Fine => new Color(0x55, 0x99, 0xFF),
+            GuildWars2.Items.Rarity.Masterwork => new Color(0x33, 0xCC, 0x11),
+            GuildWars2.Items.Rarity.Rare => new Color(0xFF, 0xDD, 0x22),
+            GuildWars2.Items.Rarity.Exotic => new Color(0xFF, 0xAA, 0x00),
             GuildWars2.Items.Rarity.Ascended => new Color(0xEE, 0x33, 0x88),
             _ => Color.White
         };
