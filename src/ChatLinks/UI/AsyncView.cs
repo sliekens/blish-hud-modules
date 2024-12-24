94c9f79948eb0cde02b70fd23808f54730b8f07a
Steven Liekens <steven.liekens@gmail.com>
2024-12-24 16:15:06 +0100

    Add AsyncView

---
+++ src/ChatLinks/UI/AsyncView.cs
@@ -0,0 +1,21 @@
+﻿using Blish_HUD.Controls;
+using Blish_HUD.Graphics.UI;
+
+namespace SL.ChatLinks.UI;
+
+public sealed class AsyncView : View
+{
+    private readonly ViewContainer _container = new();
+
+    public AsyncView(IView other)
+    {
+        _container.Show(other);
+    }
+
+    protected override void Build(Container buildPanel)
+    {
+        _container.Parent = buildPanel;
+        _container.WidthSizingMode = SizingMode.Fill;
+        _container.HeightSizingMode = SizingMode.Fill;
+    }
+}
