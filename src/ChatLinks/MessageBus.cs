6676a0038bea4a16ba06bd092bea90fc9d45476b
Steven Liekens <steven.liekens@gmail.com>
2024-12-26 07:28:55 +0100

    Add a message bus

---
+++ src/ChatLinks/MessageBus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+
+namespace SL.ChatLinks;
+
+public static class MessageBus
+{
+    private static readonly ConcurrentDictionary<string, Action<string>> Destinations = [];
+
+    public static void Send(string address, string message)
+    {
+        if (Destinations.TryGetValue(address, out var destination))
+        {
+            destination?.Invoke(message);
+        }
+    }
+
+    public static bool Register(string address, Action<string> destination)
+    {
+        return Destinations.TryAdd(address, destination);
+    }
+
+    public static bool Unregister(string address)
+    {
+        return Destinations.TryRemove(address, out _);
+    }
+}
