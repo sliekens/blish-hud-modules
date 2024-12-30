9127169e468614dc0a8fdfe25e1468f50b46bbb7
Steven Liekens <steven.liekens@gmail.com>
2024-12-30 16:23:48 +0100

    Add base class for view models

---
+++ src/Common/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace SL.Common;
+
+public abstract class ViewModel : INotifyPropertyChanging, INotifyPropertyChanged
+{
+    public event PropertyChangingEventHandler? PropertyChanging;
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanging([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+    }
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        OnPropertyChanging(propertyName);
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+}
