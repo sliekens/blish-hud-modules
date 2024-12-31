dc64c5f90fe9f9eff30bce5f4e885052c4e495ac
Steven Liekens <steven.liekens@gmail.com>
2024-12-31 16:58:49 +0100

    Add corner icon viewmodel

--- src/ChatLinks/UI/MainIcon.cs
+++ src/ChatLinks/UI/MainIcon.cs
@@ -1,17 +1,13 @@
-﻿using Blish_HUD.Content;
-using Blish_HUD.Controls;
+﻿using Blish_HUD.Controls;
 
 namespace SL.ChatLinks.UI;
 
 public sealed class MainIcon : CornerIcon
 {
-    public MainIcon()
-        : base(AsyncTexture2D.FromAssetId(155156), AsyncTexture2D.FromAssetId(155157), """
-            Chat links
-            Right-click for options
-            """)
+    public MainIcon(MainIconViewModel vm)
+        : base(vm.Texture, vm.HoverTexture, vm.Name)
     {
         Parent = Graphics.SpriteScreen;
-        Priority = 745727698;
+        Priority = vm.Priority;
     }
 }
---
+++ src/ChatLinks/UI/MainIconViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Blish_HUD.Content;
+
+using SL.Common;
+
+namespace SL.ChatLinks.UI;
+
+public class MainIconViewModel : ViewModel
+{
+    public string Name => """
+        Chat links
+        Right-click for options
+        """;
+
+    public AsyncTexture2D Texture => AsyncTexture2D.FromAssetId(155156);
+
+    public AsyncTexture2D HoverTexture => AsyncTexture2D.FromAssetId(155157);
+
+    public int Priority => 745727698;
+}
