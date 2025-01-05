48b38e4fc5fce6e3268ee46af8cb8971323e5826
Steven Liekens <steven.liekens@gmail.com>
2025-01-05 04:05:45 +0100

    Add AsyncRelayCommand

---
+++ src/Common/ModelBinding/AsyncRelayCommand.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Input;
+
+namespace SL.Common.ModelBinding;
+
+public sealed class AsyncRelayCommand(Func<Task> execute, Func<bool>? canExecute = null) : ICommand
+{
+    public bool CanExecute(object parameter)
+    {
+        return canExecute == null || canExecute();
+    }
+
+    public void Execute(object parameter)
+    {
+        execute();
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public void OnCanExecuteChanged()
+    {
+        CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
