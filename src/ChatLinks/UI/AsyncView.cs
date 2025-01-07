a856ebe707d4cdb311923fa3951c3f6af2cbc566
Steven Liekens <steven.liekens@gmail.com>
2025-01-07 18:04:41 +0100

    Fix exceptions getting lost in void

--- src/ChatLinks/UI/AsyncView.cs
+++ src/ChatLinks/UI/AsyncView.cs
@@ -3,13 +3,17 @@ using Blish_HUD.Graphics.UI;
 
 namespace SL.ChatLinks.UI;
 
-public sealed class AsyncView(Func<IView> other) : View
+public sealed class AsyncView : View
 {
     private readonly ViewContainer _container = new();
 
-    protected override Task<bool> Load(IProgress<string> progress)
+    public AsyncView(Func<IView> other)
     {
         _container.Show(other());
+    }
+
+    protected override Task<bool> Load(IProgress<string> progress)
+    {
         return Task.FromResult(true);
     }
 
