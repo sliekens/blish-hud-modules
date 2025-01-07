b17970cb622c668c9fdbdd434bb1a3a633e2d666
Steven Liekens <steven.liekens@gmail.com>
2025-01-07 21:25:21 +0100

    Add list item binding

---
+++ src/Common/ModelBinding/ListItemBinding.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq.Expressions;
+
+using SL.Common.Controls;
+
+namespace SL.Common.ModelBinding;
+
+public sealed class ListItemBinding<TViewModel, TData> : ViewModelBinding<TViewModel, bool>
+    where TViewModel : ViewModel
+{
+    public ListItem<TData> ListItem { get; }
+
+    public ListItemBinding(TViewModel viewModel, Expression<Func<TViewModel, bool>> propertySelector, ListItem<TData> listItem) : base(viewModel, propertySelector)
+    {
+        ListItem = listItem;
+        ListItem.SelectionChanged += SelectionChanged;
+        ListItem.IsSelected = Snapshot();
+    }
+
+    private void SelectionChanged(ListItem<TData> sender, ListItemSelectionChangedEventArgs args)
+    {
+        UpdateModel(args.IsSelected);
+    }
+
+    protected override void UpdateView(bool data)
+    {
+        ListItem.IsSelected = data;
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        ListItem.SelectionChanged -= SelectionChanged;
+        base.Dispose(disposing);
+    }
+}
