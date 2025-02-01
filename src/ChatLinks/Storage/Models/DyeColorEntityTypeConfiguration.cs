a84cf7568e03fffce60bf4d577536f4c4840f30b
Steven Liekens <steven.liekens@gmail.com>
2025-02-01 17:26:09 +0100

    Bump gw2sdk

---
+++ src/ChatLinks/Storage/Models/DyeColorEntityTypeConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using GuildWars2.Hero.Equipment.Dyes;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SL.ChatLinks.Storage.Models;
+
+public sealed class DyeColorEntityTypeConfiguration : IEntityTypeConfiguration<DyeColor>
+{
+    public void Configure(EntityTypeBuilder<DyeColor> builder)
+    {
+        builder.ToTable("dyes");
+        builder.HasKey(x => x.Id);
+
+    }
+}
