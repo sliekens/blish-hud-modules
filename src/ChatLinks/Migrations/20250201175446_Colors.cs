fab74a2b15cefbd4dbcacec99c65ab7871d8ee27
Steven Liekens <steven.liekens@gmail.com>
2025-02-23 11:30:01 +0100

    Format code

--- src/ChatLinks/Migrations/20250201175446_Colors.cs
+++ src/ChatLinks/Migrations/20250201175446_Colors.cs
@@ -4,59 +4,59 @@ namespace SL.ChatLinks.Migrations;
 
 public partial class Colors : Migration
 {
-	protected override void Up(MigrationBuilder migrationBuilder)
-	{
-		migrationBuilder.CreateTable(
-		   name: "Colors",
-		   columns: table => new
-		   {
-			   Id = table.Column<int>(nullable: false)
-				   .Annotation("Sqlite:Autoincrement", true),
-			   Name = table.Column<string>(nullable: false),
-			   BaseRgb = table.Column<int>(nullable: false),
-			   Cloth = table.Column<string>(nullable: false),
-			   Leather = table.Column<string>(nullable: false),
-			   Metal = table.Column<string>(nullable: false),
-			   Fur = table.Column<string>(nullable: true),
-			   ItemId = table.Column<int>(nullable: true),
-			   Hue = table.Column<string>(nullable: false),
-			   Material = table.Column<string>(nullable: false),
-			   Set = table.Column<string>(nullable: false)
-		   },
-		   constraints: table =>
-		   {
-			   table.PrimaryKey("PK_Colors", x => x.Id);
-		   });
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+           name: "Colors",
+           columns: table => new
+           {
+               Id = table.Column<int>(nullable: false)
+                   .Annotation("Sqlite:Autoincrement", true),
+               Name = table.Column<string>(nullable: false),
+               BaseRgb = table.Column<int>(nullable: false),
+               Cloth = table.Column<string>(nullable: false),
+               Leather = table.Column<string>(nullable: false),
+               Metal = table.Column<string>(nullable: false),
+               Fur = table.Column<string>(nullable: true),
+               ItemId = table.Column<int>(nullable: true),
+               Hue = table.Column<string>(nullable: false),
+               Material = table.Column<string>(nullable: false),
+               Set = table.Column<string>(nullable: false)
+           },
+           constraints: table =>
+           {
+               table.PrimaryKey("PK_Colors", x => x.Id);
+           });
 
-		migrationBuilder.CreateIndex(
-		   name: "IX_Colors_Hue",
-		   table: "Colors",
-		   column: "Hue");
+        migrationBuilder.CreateIndex(
+           name: "IX_Colors_Hue",
+           table: "Colors",
+           column: "Hue");
 
-		migrationBuilder.CreateIndex(
-		   name: "IX_Colors_ItemId",
-		   table: "Colors",
-		   column: "ItemId");
+        migrationBuilder.CreateIndex(
+           name: "IX_Colors_ItemId",
+           table: "Colors",
+           column: "ItemId");
 
-		migrationBuilder.CreateIndex(
-		   name: "IX_Colors_Material",
-		   table: "Colors",
-		   column: "Material");
+        migrationBuilder.CreateIndex(
+           name: "IX_Colors_Material",
+           table: "Colors",
+           column: "Material");
 
-		migrationBuilder.CreateIndex(
-		   name: "IX_Colors_Name",
-		   table: "Colors",
-		   column: "Name");
+        migrationBuilder.CreateIndex(
+           name: "IX_Colors_Name",
+           table: "Colors",
+           column: "Name");
 
-		migrationBuilder.CreateIndex(
-		   name: "IX_Colors_Set",
-		   table: "Colors",
-		   column: "Set");
-	}
+        migrationBuilder.CreateIndex(
+           name: "IX_Colors_Set",
+           table: "Colors",
+           column: "Set");
+    }
 
-	protected override void Down(MigrationBuilder migrationBuilder)
-	{
-		migrationBuilder.DropTable(
-		   name: "Colors");
-	}
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+           name: "Colors");
+    }
 }
--- src/ChatLinks/Migrations/20250201221800_JadeBots.cs
+++ src/ChatLinks/Migrations/20250201221800_JadeBots.cs
@@ -4,37 +4,37 @@ namespace SL.ChatLinks.Migrations;
 
 public partial class JadeBots : Migration
 {
-	protected override void Up(MigrationBuilder migrationBuilder)
-	{
-		migrationBuilder.CreateTable(
-		   name: "JadeBots",
-		   columns: table => new
-		   {
-			   Id = table.Column<int>(nullable: false)
-				   .Annotation("Sqlite:Autoincrement", true),
-			   Name = table.Column<string>(nullable: false),
-			   Description = table.Column<string>(nullable: false),
-			   UnlockItemId = table.Column<int>(nullable: false)
-		   },
-		   constraints: table =>
-		   {
-			   table.PrimaryKey("PK_JadeBots", x => x.Id);
-		   });
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+           name: "JadeBots",
+           columns: table => new
+           {
+               Id = table.Column<int>(nullable: false)
+                   .Annotation("Sqlite:Autoincrement", true),
+               Name = table.Column<string>(nullable: false),
+               Description = table.Column<string>(nullable: false),
+               UnlockItemId = table.Column<int>(nullable: false)
+           },
+           constraints: table =>
+           {
+               table.PrimaryKey("PK_JadeBots", x => x.Id);
+           });
 
-		migrationBuilder.CreateIndex(
-		   name: "IX_JadeBots_Name",
-		   table: "JadeBots",
-		   column: "Name");
+        migrationBuilder.CreateIndex(
+           name: "IX_JadeBots_Name",
+           table: "JadeBots",
+           column: "Name");
 
-		migrationBuilder.CreateIndex(
-		   name: "IX_JadeBots_UnlockItemId",
-		   table: "JadeBots",
-		   column: "UnlockItemId");
-	}
+        migrationBuilder.CreateIndex(
+           name: "IX_JadeBots_UnlockItemId",
+           table: "JadeBots",
+           column: "UnlockItemId");
+    }
 
-	protected override void Down(MigrationBuilder migrationBuilder)
-	{
-		migrationBuilder.DropTable(
-		   name: "JadeBots");
-	}
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+           name: "JadeBots");
+    }
 }
