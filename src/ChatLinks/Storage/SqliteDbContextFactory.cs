91c8be547b9dfd61e3987ad9ccca1efa08f96098
Steven Liekens <steven.liekens@gmail.com>
2025-03-29 17:40:15 +0100

    Restore EF core logging

--- src/ChatLinks/Storage/SqliteDbContextFactory.cs
+++ src/ChatLinks/Storage/SqliteDbContextFactory.cs
@@ -2,15 +2,17 @@
 
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace SL.ChatLinks.Storage;
 
-public class SqliteDbContextFactory(IOptions<DatabaseOptions> options) : IDbContextFactory
+public class SqliteDbContextFactory(IOptions<DatabaseOptions> options, ILoggerFactory loggerFactory) : IDbContextFactory
 {
     public ChatLinksContext CreateDbContext(Language language)
     {
         DbContextOptionsBuilder<ChatLinksContext> optionsBuilder = new();
+        _ = optionsBuilder.UseLoggerFactory(loggerFactory);
 #pragma warning disable CA2000 // Dispose objects before losing scope
         SqliteConnection connection = new(options.Value.ConnectionString(language));
 #pragma warning restore CA2000 // Dispose objects before losing scope
@@ -22,6 +24,7 @@ public class SqliteDbContextFactory(IOptions<DatabaseOptions> options) : IDbCont
     public ChatLinksContext CreateDbContext(string file)
     {
         DbContextOptionsBuilder<ChatLinksContext> optionsBuilder = new();
+        _ = optionsBuilder.UseLoggerFactory(loggerFactory);
 #pragma warning disable CA2000 // Dispose objects before losing scope
         SqliteConnection connection = new(options.Value.ConnectionString(file));
 #pragma warning restore CA2000 // Dispose objects before losing scope
