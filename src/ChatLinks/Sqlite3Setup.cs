977fe51db067214bc088190746ab643dec4f77fd
Steven Liekens <steven.liekens@gmail.com>
2025-12-06 22:54:04 +0100

    Use SourceGear sqlite

--- src/ChatLinks/Sqlite3Setup.cs
+++ src/ChatLinks/Sqlite3Setup.cs
@@ -6,7 +6,8 @@ public static class Sqlite3Setup
 {
     public static void Run()
     {
-        SQLite3Provider_dynamic_cdecl.Setup("e_sqlite3", new ModuleGetFunctionPointer("sliekens.e_sqlite3"));
-        raw.SetProvider(new SQLite3Provider_dynamic_cdecl());
+        Batteries_V2.Init();
+        //SQLite3Provider_dynamic_cdecl.Setup("e_sqlite3", new ModuleGetFunctionPointer("sliekens.e_sqlite3"));
+        //raw.SetProvider(new SQLite3Provider_dynamic_cdecl());
     }
 }
