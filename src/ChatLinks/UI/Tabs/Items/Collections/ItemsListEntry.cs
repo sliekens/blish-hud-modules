19594ff7dee3428737c2c939a1fe12fd134e1da3
Steven <steven.liekens@gmail.com>
2026-01-19 22:58:23 +0100

    Use scrolling highlight effect in items lists

--- src/ChatLinks/UI/Tabs/Items/Collections/ItemsListEntry.cs
+++ src/ChatLinks/UI/Tabs/Items/Collections/ItemsListEntry.cs
@@ -1,6 +1,7 @@
 ﻿using Blish_HUD;
 using Blish_HUD.Content;
 using Blish_HUD.Controls;
+using Blish_HUD.Controls.Effects;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -9,33 +10,35 @@ using SL.ChatLinks.UI.Tabs.Items.Tooltips;
 
 namespace SL.ChatLinks.UI.Tabs.Items.Collections;
 
-public sealed class ItemsListEntry(ItemsListViewModel viewModel) : Control
+public sealed class ItemsListEntry : Control
 {
-    private static readonly Color ActiveColor = new(109, 100, 69, 0);
+    private readonly ScrollingHighlightEffect _highlightEffect;
 
-    private static readonly Color HoverColor = new(109, 100, 69, 127);
-
-    private readonly AsyncTexture2D? _icon = viewModel.GetIcon();
+    private readonly AsyncTexture2D? _icon;
 
     private readonly Rectangle _iconBounds = new(0, 0, 35, 35);
-
+    private readonly ItemsListViewModel _viewModel;
     private Rectangle _textBounds = Rectangle.Empty;
+
+    public ItemsListEntry(ItemsListViewModel viewModel)
+    {
+        ThrowHelper.ThrowIfNull(viewModel);
+        _viewModel = viewModel;
+        _icon = viewModel.GetIcon();
+        _highlightEffect = new(this);
+        EffectBehind = _highlightEffect;
+    }
 
     public override void DoUpdate(GameTime gameTime)
     {
-        BackgroundColor = viewModel.IsSelected switch
-        {
-            true => ActiveColor,
-            false when MouseOver => HoverColor,
-            _ => Color.Transparent
-        };
 
         if (MouseOver)
         {
-            Tooltip ??= new Tooltip(new ItemTooltipView(viewModel.CreateTooltipViewModel()));
+            Tooltip ??= new Tooltip(new ItemTooltipView(_viewModel.CreateTooltipViewModel()));
         }
         else
         {
+            _highlightEffect.ForceActive = _viewModel.IsSelected;
             Tooltip?.Dispose();
             Tooltip = null;
         }
@@ -61,13 +64,13 @@ public sealed class ItemsListEntry(ItemsListViewModel viewModel) : Control
             spriteBatch.DrawOnCtrl(this, _icon, _iconBounds, Color.White);
         }
 
-        if (MouseOver || viewModel.IsSelected)
+        if (MouseOver || _viewModel.IsSelected)
         {
             foreach ((int x, int y) in (ReadOnlySpan<(int, int)>)[(1, 1), (-1, 1), (-1, -1), (1, -1)])
             {
                 spriteBatch.DrawStringOnCtrl(
                     this,
-                    viewModel.ItemName,
+                    _viewModel.ItemName,
                     Content.DefaultFont14,
                     _textBounds.OffsetBy(x, y),
                     new Color(Color.Black, .4f),
@@ -78,10 +81,10 @@ public sealed class ItemsListEntry(ItemsListViewModel viewModel) : Control
 
         spriteBatch.DrawStringOnCtrl(
             this,
-            viewModel.ItemName,
+            _viewModel.ItemName,
             Content.DefaultFont14,
             _textBounds,
-            viewModel.Color,
+            _viewModel.Color,
             true
         );
     }
